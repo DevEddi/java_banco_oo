d682ec25758729dd199be2c79a4e18f4525bc2a9
Edmilson Scovino <scovinowork@gmail.com>
2021-04-03 12:48:11 -0300

    Classes Cliente e Endereco

---
+++ src/Dados_cliente_banco/Cliente.java
@@ -0,0 +1,61 @@
+package Dados_cliente_banco;
+
+import java.util.Date;
+
+public class Cliente {
+
+    private String CPF;
+    private String Nome;
+    private String telefone;
+    private String sexo;
+    private String naturalidade;
+    private Date nascimento;
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNaturalidade() {
+        return naturalidade;
+    }
+
+    public void setNaturalidade(String naturalidade) {
+        this.naturalidade = naturalidade;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+}
